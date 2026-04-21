722733ad4be831016bcd98e46146ebd5656a3051
luozz23 <luozz23@meicloud.com>
2026-04-21 15:18:49 +0800

    添加工程化配置

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,25 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import eslintPlugin from 'vite-plugin-eslint2'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
     vueDevTools(),
+    eslintPlugin({
+      cache: false,
+      include: ['src/**/*.vue', 'src/**/*.js'],
+      emitErrorAsWarning: false,
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
